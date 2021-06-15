5790736b92e90d72f721f10a5d393e79c9ba853f
CEED87 <ceed210787@gmail.com>
2021-06-15 20:27:28 +0300

    slide_finish

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 var container = document.querySelector('.container');
-var activeFoto = document.querySelector('.photo');
-// var foto = activeFoto
+var photo = document.querySelector('img');
 
-container.addEventListener('click',function(event) {
-    var slideImg = event.target;
-    var active = container.querySelector('.active-image');
-      activeFoto.lastChild.remove();
-    if (slideImg.parentNode.className === 'image') {
-        var clone = slideImg.cloneNode();
-        activeFoto.replaceChild(clone, activeFoto.lastChild)
-        //  activeFoto.lastChild.remove();
-        active.classList.remove('active-image');
-        slideImg.parentNode.classList.add('active-image');
-       
-        // activeFoto.lastChild.remove();
- console.log(activeFoto.firstChild)
-    }
-       
-    });
-    // if (slideImg.parentNode.className === 'active-image') {
-        
-    // console.log(slideImg.parentNode);
-// });
+container.addEventListener('click', function (event) {
+  var slideImg = event.target;
+  var active = container.querySelector('.active-image');
 
-// if ()
-
-
+  if (slideImg.parentNode.className === 'image') {
+    var attribute = slideImg.getAttribute('src');
+    active.classList.remove('active-image');
+    slideImg.parentNode.classList.add('active-image');
+    photo.setAttribute('src', attribute);
+  }
+});
