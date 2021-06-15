107854b16a455244ec7f3dbb877e078b98782361
CEED87 <ceed210787@gmail.com>
2021-06-15 17:27:12 +0300

    slide_ dont work

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 var container = document.querySelector('.container');
 var activeFoto = document.querySelector('.photo');
+// var foto = activeFoto
 
 container.addEventListener('click',function(event) {
     var slideImg = event.target;
+    var active = container.querySelector('.active-image');
+      activeFoto.lastChild.remove();
     if (slideImg.parentNode.className === 'image') {
-        slideImg.classList.add('active-image');
+        var clone = slideImg.cloneNode();
+        activeFoto.replaceChild(clone, activeFoto.lastChild)
+        //  activeFoto.lastChild.remove();
+        active.classList.remove('active-image');
+        slideImg.parentNode.classList.add('active-image');
+       
+        // activeFoto.lastChild.remove();
+ console.log(activeFoto.firstChild)
     }
-    console.log(slideImg.parentNode);
-});
+       
+    });
+    // if (slideImg.parentNode.className === 'active-image') {
+        
+    // console.log(slideImg.parentNode);
+// });
+
+// if ()
 
 
